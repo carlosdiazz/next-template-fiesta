fc9284bfe665b906d7c91bcc60d0e719866c560d
Carlos DIaz <c.diazadriann@gmail.com>
2024-08-09 08:09:35 -0400

    Agregue Auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,40 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "******",
+        },
+      },
+      authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+        const { username, password } = credentials;
+
+        if (username === "pablo@orkapi.com" && password === "Pabl0Vidal99") {
+          const user = {
+            id: "1",
+            name: "Pablo",
+            username: username,
+          };
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {},
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
